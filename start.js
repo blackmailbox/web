f066f233b55d8585ea06f5ad4d8234847f70876d
Ben Reinhart <breinhart@groupon.com>
2013-03-02 17:08:35 -0800

    Better naming

--- start.js
+++ start.js
@@ -2,9 +2,9 @@ require('coffee-script');
 
 var exec = require('child_process').exec;
 
-exec('pwd', function(err, response){
-  response = response.replace(/\n/, '');
-  process.env.APP_ROOT = response;
+exec('pwd', function(err, cwd){
+  cwd = cwd.replace(/\n/, '');
+  process.env.APP_ROOT = cwd;
 
   require(process.env.APP_ROOT + '/core/server');
 });
